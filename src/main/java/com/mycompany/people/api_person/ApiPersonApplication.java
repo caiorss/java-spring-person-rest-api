3a70f465e4099f63f2c9be8faba50fd107845b6b
CaioRS <caiorss.rodrigues@gmail.com>
2021-06-29 14:06:06 -0300

    Reformat class ApiPersonApplication.

--- src/main/java/com/mycompany/people/api_person/ApiPersonApplication.java
+++ src/main/java/com/mycompany/people/api_person/ApiPersonApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiPersonApplication {
+public class ApiPersonApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println(" [INFO] Hello world! ");
 		SpringApplication.run(ApiPersonApplication.class, args);
 	}
 
