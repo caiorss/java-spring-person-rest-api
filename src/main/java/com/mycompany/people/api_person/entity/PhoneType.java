f93550102246644e9ac6cff4d63749b791ab60e5
CaioRS <caiorss.rodrigues@gmail.com>
2021-06-29 14:31:47 -0300

    Implement enumeration PhoneType.

--- src/main/java/com/mycompany/people/api_person/entity/PhoneType.java
+++ src/main/java/com/mycompany/people/api_person/entity/PhoneType.java
@@ -1,5 +1,15 @@
 package com.mycompany.people.api_person.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PhoneType
 {
+      HOME("HOME")
+    , COMMERCIAL("COMMERCIAL")
+    , MOBILE("MOBILE");
+
+    private final String type;
 }
