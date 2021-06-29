8eedd053f5d57bbfccaab729fe120bc93c001d23
CaioRS <caiorss.rodrigues@gmail.com>
2021-06-29 13:51:42 -0300

    Started project.

---
+++ src/main/java/com/mycompany/people/api_person/ApiPersonApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.people.api_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/people/api_person/ApiPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.people.api_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
