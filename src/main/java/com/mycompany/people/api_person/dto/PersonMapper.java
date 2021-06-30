2f01261bfa64777d2c4550fe1591666c209a38a3
CaioRS <caiorss.rodrigues@gmail.com>
2021-06-30 10:53:57 -0300

    Add explanation comment to class PersonMapper.

--- src/main/java/com/mycompany/people/api_person/dto/PersonMapper.java
+++ src/main/java/com/mycompany/people/api_person/dto/PersonMapper.java
@@ -10,7 +10,9 @@ import org.mapstruct.Mapping;
 @Mapper( componentModel = "spring" )
 public interface PersonMapper
 {
-    // @Mapping(target = "dateOfBirth", source = "dateOfBirth", dateFormat = "yyyy-MM-dd")
+    // Note: dateOfBirth (data de nascimento - in Portuguse) is in the format ISO 8601
+    // where yyyy is the year, MM is the month (numeric) 2 digits and dd is the date.
+    @Mapping(target = "dateOfBirth", source = "dateOfBirth", dateFormat = "yyyy-MM-dd")
     Person toModel(PersonDTO dto);
 
     PersonDTO toDto(PersonDTO dto);
