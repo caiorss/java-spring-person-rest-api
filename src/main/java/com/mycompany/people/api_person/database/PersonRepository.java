a400d9546ef6f5c1c54728981f710457df252ec2
CaioRS <caiorss.rodrigues@gmail.com>
2021-07-11 16:29:20 -0300

    Implement PersonRepository.findPersonByLastName() method.

--- src/main/java/com/mycompany/people/api_person/database/PersonRepository.java
+++ src/main/java/com/mycompany/people/api_person/database/PersonRepository.java
@@ -1,9 +1,16 @@
 package com.mycompany.people.api_person.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 // Person is the type of data managed by the repository
 // Long is the type of id (primary key) used by the repository
 public interface PersonRepository extends JpaRepository<Person, Long>
 {
+    // Query using JPQL ( Java Persistence Query Language)
+    @Query("SELECT p FROM Person p WHERE lower(p.lastName) LIKE lower( concat('%', :lastName, '%') ) ")
+    List<Person> findPersonByLastName(@Param("lastName") String lastName );
 }
